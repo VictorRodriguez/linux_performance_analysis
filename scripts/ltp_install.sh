ae209035572ddf518717e678a1d84541db797677
helojagt <helojagt@problem2-PC.lan>
2015-02-10 22:41:05 -0600

    LTP script installation

---
+++ scripts/ltp_install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+#Checking de minimum dependencies to install LTP
+
+
+packageInstalling () {
+
+    if [ ! hash $1 2>/dev/null ] ; then
+
+	sudo ./install_bison.sh
+
+    
+	# This if will check if the corresponding package were correctly installed
+    
+	if [ ! $? ]; then
+
+	    echo *****Installation of $1 failed. Quitting...
+	    exit 1
+
+	fi
+
+    fi
+
+}
+
+
+echo "*****************************************"
+echo "** Welcome to the installation of LTP  **"
+echo "*****************************************"
+
+
+
+
+# Packages Installing
+packageInstalling bison
+
+packageInstalling byacc
+
+packageInstalling flex
+
+packageInstalling make
+
+packageInstalling automake
+
+packageInstalling autoconf
+
+packageInstalling m4
+
+
+#Will download the LTP source on the current directory
+wget wget -O ltp-full-20150119.tar.bz2 http://sourceforge.net/projects/ltp/files/latest/download?source=typ_redirect
+
+
+#Decompress the archive
+
+tar xvjf ltp-full-20150119.tar.bz2
+
+cd ltp*
+
+#Configure the source code using autotools
+make autotools
+./configure
+
+if [ ! $? ] ; then
+
+    exit 1
+
+fi
+
+#Installing LTP
+make all
+sudo make SKIP_IDCHECK=1 install
+
+echo "The installation has been succesfull, try \"runalltest\" script which is located in /opt/ltp directory. Have fun!!!"
+
